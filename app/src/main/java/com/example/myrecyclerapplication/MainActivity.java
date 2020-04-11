5573099047996f9cbaee6ce1b9029b09e7a4b3c1
PutraHarry <putraharry19@gmail.com>
2020-04-11 21:52:08 +0800

    sudah tercommit

---
+++ app/src/main/java/com/example/myrecyclerapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myrecyclerapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.chrome, R.drawable.edge, R.drawable.firefox,
+            R.drawable.opera, R.drawable.safari, R.drawable.tor, R.drawable.uc, R.drawable.vivaldi, R.drawable.profil};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.web_browser);
+        s2 = getResources().getStringArray(R.array.descriptiona);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
